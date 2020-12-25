46bdbb61a40b3783faa7ae941ef9f3e1c1d79e77
simsalabim08 <westra77@gmail.com>
2020-12-25 18:54:44 +0100

    Add user validations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,3 @@
 class AdminUser < User
-  
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+require 'support/factory_bot'
 
 include Warden::Test::Helpers
 Warden.test_mode!
@@ -13,7 +14,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
   config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
   config.before(:each) { DatabaseCleaner.strategy = :transaction }
   config.before(:each, :js => true) { DatabaseCleaner.strategy = :truncation }
@@ -21,5 +22,5 @@ RSpec.configure do |config|
   config.after(:each) { DatabaseCleaner.clean }
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end
