2b2fcec32b87e0a2a153dfa0451aca19d760434d
simsalabim08 <westra77@gmail.com>
2020-12-26 00:20:08 +0100

    Implemented layout for homepage and nav

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,14 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "bootstrap"
 
+document.addEventListener("Turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require jquery
-//= require bootstrap-sprockets
-//= require jquery_ujs
-//= require_tree .
