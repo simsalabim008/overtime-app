00d1679db84e0e20b69403e87738a85525c1e828
simsalabim08 <westra77@gmail.com>
2020-12-25 22:05:01 +0100

    Implemented user connection with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,8 @@ describe 'navigate' do
 
   describe 'creation' do
     before do
+      user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Snow")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
@@ -22,10 +24,10 @@ describe 'navigate' do
     end
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "Some rationale"
+      fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
 
-      expect(page).to have_content("Some rationale")
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
